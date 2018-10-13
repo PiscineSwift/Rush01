a1c9bc281a093d2cb5c099eb1550667ade1ba324
twix <twix@inernetbs.net>
2018-10-13 14:08:57 +0300

    Remove useless SwiftLocation and add GoogleMaps

--- rush01/rush01/ViewController.swift
+++ rush01/rush01/ViewController.swift
@@ -9,17 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        Locator.requestAuthorizationIfNeeded(.whenInUse)
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
