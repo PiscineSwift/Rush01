0821118f2af94ddf79ef047be8c375f89bc226d2
twix <twix@inernetbs.net>
2018-10-13 13:05:19 +0300

    Initial commit

---
+++ rush01/rush01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rush01
+//
+//  Created by Vitalii Poltavets on 10/13/18.
+//  Copyright © 2018 Vitalii Poltavets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
